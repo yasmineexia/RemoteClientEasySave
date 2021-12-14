fb7da47540692eb3655e90f1021acc780b840384
yasmineexia <yasmine.benoudina.dz@viacesi.fr>
2021-12-14 23:28:08 +0000

    Structure

--- RemoteClientEasySave/Views/MainWindow.xaml.cs
+++ RemoteClientEasySave/Views/MainWindow.xaml.cs
@@ -10,19 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RemoteClientEasySave
+namespace RemoteClientEasySave.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            
         }
     }
 }
